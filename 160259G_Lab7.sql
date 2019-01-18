7c85478274e0f75ad236c87beeb0fd289b45c280
chaminnk <36238418+chaminnk@users.noreply.github.com>
2019-01-18 08:22:23 +0530

    Update 160259G_Lab7.sql

--- 160259G_Lab7.sql
+++ 160259G_Lab7.sql
@@ -1,7 +1,17 @@
 -- 1 
+
 create table if not exists grade_point (
-	grade varchar(8) primary key,
-	point float(2,1)
+
+	grade varchar(8) primary key,	
+
 );
+
+Hddjfhcjxbjaaksnsb
+
+Gahdbrdjiffj
+
+Ueuddhufufudbsb
+
 -- 2
+
 insert into grade_point (grade,point) values ('A+',4.2),('A',4.0),('A-',3.7),('B+',3.5),('B',3.0),('B-',2.7),('C+',2.3),('C',2.0),('C-',1.5),('D',1.0);
